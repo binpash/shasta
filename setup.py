b9114fb10a80b0b35e6d4854581b62477c76f980
Konstantinos Kallas <konstantinos.kallas@hotmail.com>
2026-01-18 12:31:50 +0100

    Change version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from pathlib import Path
 long_description = (Path(__file__).parent / "README.md").read_text()
 
 setup(name='shasta',
-      version='0.2.2',
+      version='0.3.1',
       packages=['shasta'],
       ## Necessary for the markdown to be properly rendered
       long_description=long_description,
